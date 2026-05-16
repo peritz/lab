2ac2d6daedc2ca7cfedcdeea7e8bbbdda40d9ea7
peritz <peritz@pardonmynoot.com>
2026-05-16 16:16:39 +0100

    Add basic templating with bootstrap

--- flask/main.py
+++ flask/main.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return "<h1>Hello, World!</h1>"
+
+@app.route('/user/<username>')
+def get_user(username):
+    return render_template('user.html', username=username)
+
+@app.route('/basic')
+def basic():
+    return render_template('basic.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
