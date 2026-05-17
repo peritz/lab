db24a9292b83b19067b01647c498237687b83d63
peritz <peritz@pardonmynoot.com>
2026-05-17 11:26:50 +0100

    Add a form

--- flask/main.py
+++ flask/main.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
 
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "thisismysecretkey"
 
 @app.route('/')
 def hello_world():
@@ -13,6 +17,23 @@ def get_user(username):
 @app.route('/basic')
 def basic():
     return render_template('basic.html')
+
+class UserForm(FlaskForm):
+    name = StringField("Name")
+    email = StringField("Email")
+    submit = SubmitField("Submit")
+
+@app.route('/form', methods=["GET", "POST"])
+def form():
+    name = False
+    email = False
+    form = UserForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ""
+        email = form.email.data
+        form.email.data = ""
+    return render_template("form.html", form=form, name=name, email=email)
 
 if __name__ == '__main__':
     app.run(debug=True)
